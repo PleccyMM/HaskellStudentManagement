bb388a47fbf4c04385e3e7f6ebbc08bb6b5db830
PleccyMM <pleccymm@outlook.com>
2024-04-25 12:27:22 +0100

    Adding students possible

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,21 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
 
 module Main (main) where
 
 import Lib
-import Data.Aeson
 import GHC.Generics
-import Data.Text (Text)
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO as I
+import Data.Aeson (ToJSON, FromJSON, eitherDecode)
+import Data.Aeson.Text (encodeToLazyText)
 import qualified Data.ByteString.Lazy as B
-import Control.Monad (mzero)
 
 data Student = Student {
     firstName :: !Text,
     secondName :: !Text,
     age :: Int,
     modules :: [Text]
-} deriving (Show, Generic)
-
-instance FromJSON Student
-instance ToJSON Student
+} deriving (Show, Generic, ToJSON, FromJSON)
 
 jsonFile :: FilePath
 jsonFile = "test.json"
@@ -29,5 +27,17 @@ main :: IO ()
 main = do
     d <- (eitherDecode <$> getJSON) :: IO (Either String [Student])
     case d of
-        Left err -> putStrLn err
+        Left err -> Prelude.putStrLn err
         Right st -> print st
+            
+
+
+addStudent :: IO ()
+addStudent = do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Student])
+        case d of
+            Left err -> Prelude.putStrLn err
+            Right x -> do
+                let st = Student { firstName = "Jessica", secondName = "Lovegood", age = 22, modules = ["BS0021", "MD3990"] }
+                    updatedStudents = st : x
+                I.writeFile jsonFile $ encodeToLazyText updatedStudents
