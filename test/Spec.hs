c892ee0496b5e2e9a83a04a63ed21b4d85cada58
PleccyMM <pleccymm@outlook.com>
2024-05-07 11:38:50 +0100

    Before refactor

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,9 +12,14 @@ main = do
 testSearchEmpty :: Test
 testSearchEmpty = TestCase ( assertEqual "search empty file" expected (findStudents input) )
     where input = ""
-          expected = IO (Nothing)
+        expected = testFunc Nothing
 
 testSearchRealFirst :: Test
 testSearchRealFirst = TestCase ( assertEqual "search for an existing student by first name" expected (findStudents input) )
     where input = "Reuben"
-          expected = Just (Student { firstName = "Reuben", secondName = "Shaw", age = 20, year = 2, modules = ["BS2201","BS2202","BS2220","BS2221"] })
+        expected = testFunc (Just (Student { firstName = "Reuben", secondName = "Shaw", age = 20, year = 2, modules = ["BS2201","BS2202","BS2220","BS2221"] }))
+
+testFunc :: (Show a) => a -> IO a
+testFunc i = do
+    r <- i
+    return (r)
